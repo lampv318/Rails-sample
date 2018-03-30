f7aa71cd4672ac180ced41438510df56f891d503
lampv <lampvhy@gmail.com>
2018-03-30 11:10:19 +0700

    modeling user

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :name,  presence: true, length: {maximum: Settings.max_name}
+  validates :email, presence: true, length: {maximum: Settings.max_email},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.min_pass}
+
+  before_save :email_downcase
+
+  has_secure_password
+
+  private 
+  def email_downcase
+    email.downcase!
+  end
+end
